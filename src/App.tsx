f1dbb1de05ba5aba34a36b0500c561609cd6e259
Ola Czyrnek <98264760+olcolcolc@users.noreply.github.com>
2023-07-13 16:27:56 +0200

    feat(icon): Add icon component. (#7)

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
+import { Icon } from "./components/icon/Icon";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <h1>vrs</h1>
+      <div>
+        <a href="https://www.facebook.com/veganramenshop" target="_blank">
+          <Icon name="facebook" />
+        </a>
+        <a href="https://www.instagram.com/veganramenshop" target="_blank">
+          <Icon name="instagram" />
+        </a>
+      </div>
     </>
   );
 }
