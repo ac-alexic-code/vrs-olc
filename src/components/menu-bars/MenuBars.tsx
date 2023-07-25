5b256ba4f6d99b1f86ab11754b5b5eb64e0621c0
Ola Czyrnek <aleksandra@czyrnek.net>
2023-07-25 22:05:37 +0200

    feat(navbar): Add rwd to MenuBars component

--- src/components/menu-bars/MenuBars.tsx
+++ src/components/menu-bars/MenuBars.tsx
@@ -1,6 +1,21 @@
+import styled from "@emotion/styled";
 import { Icon } from "../icon/Icon";
 
+const MenuBarsContainer = styled.div`
+  display: block;
+  ${({ theme }) =>
+    theme.mixins.forDesktop(`
+    display: none;
+
+    `)}
+`;
+
 const MenuBars: React.FC = () => {
-  return <Icon name="menuBars" />;
+  return (
+    <MenuBarsContainer>
+      <Icon name="menuBars" />
+    </MenuBarsContainer>
+  );
 };
+
 export default MenuBars;
